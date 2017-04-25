b7150e7a38b6b9319515d2394fdefd4ff15241c1
Vitaliy Uvarov <vitalyu@MacBook-Air-Vitaliy.local>
2017-04-25 18:43:52 +0300

    cross compiled

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPTDIR=$( cd $(dirname $0); pwd )
 logslist=$( cat "${SCRIPTDIR}/config/logslist" | tr '\n' ',' )
-${SCRIPTDIR}/bin/mtail --progs "${SCRIPTDIR}/config/programs-enabled" --logs $logslist
+mtail=mtail_linux64
+${SCRIPTDIR}/bin/${mtail} --progs "${SCRIPTDIR}/config/programs-enabled" --logs $logslist
